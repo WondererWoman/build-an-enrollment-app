3810b8a28accd2ad190beeb55799e3760f2970cf
Erin DeVries <erindevrieswebdev@gmail.com>
2017-03-17 16:02:14 -0500

    Created User and Course models. Created render functions for mainpage and browse

---
+++ models.py
@@ -0,0 +1,16 @@
+from google.appengine.ext import db
+
+class User(db.Model):
+    username = db.StringProperty(required = True)
+    pw_hash = db.StringProperty(required = True)
+    first_name = db.StringProperty(required = True)
+    last_name = db.StringProperty(required = True)
+    phone_num = db.PhoneNumberProperty(required = True)
+    email = db.EmailProperty()
+    address = db.PostalAddressProperty()
+
+class Course(db.Model):
+    name = db.StringProperty(required = True)
+    description = db.TextProperty(required = True)
+    instructor = db.StringProperty(required = True)
+    
